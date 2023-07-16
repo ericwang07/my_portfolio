29ad275fed7685420983125c2450bddc35e42ac6
ewang001 <eric.y.wang@yale.edu>
2023-07-16 14:20:41 -0700

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,22 +15,22 @@ const About = () => {
               I believe that intuitive interfaces are the key to maximize the potential of the most cutting-edge technologies.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              I began my journey at the age of twelve, when I received a free netbook in exchange for the completion of an online course meant to increase education access in low-income areas. 
+              I began my journey at the age of twelve, when I received a free netbook in exchange for the completion of an online course made to increase education access in low-income areas. 
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
               Tired of being limited by my local public library's daily thirty minutes of computer time, I cherished the netbook, indulging in the world of computing. It was a luxury my family couldn't afford.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              Pushing the netbook beyond its hardware limitations excited me, as I shaped it into a capable device.
+              Pushing the netbook beyond its hardware limitations excited me, as I eventually shaped it from an virtually-unusable toy to a capable device.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
               For my family flea market business, it allowed us to tap into the world of online wholesale. Our profits tripled that year.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              Inspired by the positive influence I was able to make for my family, I forge ahead.
+              Motivated by seeing the impact I can make for others around me, I forge ahead.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              My purpose is to innovate accessible machine-learning tools, blending technology and empathy to shape a future where seamless human-machine harmony prevails.
+              I am on a path to innovate accessible machine-learning tools, blending technology and empathy to shape a future where seamless human-machine harmony prevails.
             </div>            
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>
               Flea market vendor, lefty violinist, stoic philosophy reader, and tennis lover.
