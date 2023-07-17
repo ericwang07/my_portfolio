703652cc5eef1175db833593c0b3bea6b97d9e81
ewang001 <eric.y.wang@yale.edu>
2023-07-16 17:32:56 -0700

    changed about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,10 +18,10 @@ const About = () => {
               I began my journey at the age of twelve, when I received a free netbook in exchange for the completion of an online course made to increase education access in low-income areas.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 ml-auto mr-0 text-gray-400 flex'>  
-              I cherished the netbook, and it allowed me to indulge in the luxury of computer-tinkering; a luxury my parents couldn't afford to provide me.
+              I cherished the netbook, as it allowed me to indulge in the luxury of computer-tinkering; a luxury my parents couldn't afford to provide me.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 ml-auto mr-0 text-gray-400 flex'>  
-              Pushing the netbook beyond its hardware limitations excited me, as I eventually shaped it from an virtually-unusable toy to a seriously capable device.
+              Pushing the netbook beyond its hardware limitations excited me, as I eventually shaped it from a cheap toy to a seriously capable device.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 ml-auto mr-0 text-gray-400 flex'>  
               For my family flea market business, it opened up our gates to the world of online wholesale. Our profits tripled that year.
