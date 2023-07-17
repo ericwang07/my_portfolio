544152239d9bba1c13e20e58c527e10522a05319
ewang001 <eric.y.wang@yale.edu>
2023-07-16 17:17:02 -0700

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,22 +12,22 @@ const About = () => {
               Born in 2003 in Chicago, Illinois. Based in Saint Paul, Minnesota.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>
-              I believe that intuitive interfaces are the key to maximize the potential of the most cutting-edge technologies.
+              I believe that intuitive interfaces are the key to maximize the positive potential of cutting-edge technologies.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              I began my journey at the age of twelve, when I received a free netbook in exchange for the completion of an online course made to increase education access in low-income areas. 
+              I began my journey at the age of twelve, when I received a free netbook in exchange for the completion of an online course made to increase education access in low-income areas.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              Tired of being limited by my local public library's daily thirty minutes of computer time, I cherished the netbook, indulging in the world of computing. It was a luxury my family couldn't afford.
+              I cherished the netbook, and it allowed me to indulge in the luxury of computer-tinkering; a luxury my parents couldn't afford to provide me.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              Pushing the netbook beyond its hardware limitations excited me, as I eventually shaped it from an virtually-unusable toy to a capable device.
+              Pushing the netbook beyond its hardware limitations excited me, as I eventually shaped it from an virtually-unusable toy to a seriously capable device.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              For my family flea market business, it allowed us to tap into the world of online wholesale. Our profits tripled that year.
+              For my family flea market business, it opened up the gates to the world of online wholesale. Our profits tripled that year.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
-              Motivated by seeing the impact I can make for others around me, I forge ahead.
+              Believing in my ability to harness technology to help those around me, I forge ahead.
             </div>
             <div className='font-2xl text-sm pb-4 w-1/3 text-gray-400 flex'>  
               I am on a path to innovate accessible machine-learning tools, blending technology and empathy to shape a future where seamless human-machine harmony prevails.
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,7 +16,7 @@ const Portfolio = () => {
     {
       id: 1,
       title: "YCAL",
-      description: "A social calendar app built for Yale students. Features Yale course integration, friending, group events, and AI scheduler.",
+      description: "A social calendar app built for Yale students that allows you to seamlessly coordinate your rigorous academic plans with your vibrant social life. Yale course integration makes the course-shopping process easier, allowing you to instantly view how a course would conflict with your other commitments. You can add friends and view their calendars, create group events, and ask the AI scheduler to peek at every group member's calendars to find the best time for a boba study break.",
       languages: "Javascript, HTML, CSS",
       technologies: "MongoDB, ExpressJS, React, NodeJS",
       src: ycalImage,
@@ -26,7 +26,7 @@ const Portfolio = () => {
     {
       id: 2,
       title: "SANTORINI",
-      description: "A command-line representation of the Santorini board game. Human, random, or heuristic (pseudo-AI) player modes. Undo/redo and game stats functionality are supported.",
+      description: "A command-line representation of the Santorini board game. Human, random, or heuristic player modes. Undo/redo and score displaying functionality are supported. The heuristic player mode exercises a primitive form of AI that chooses the best move. Utilizes design patterns (i.e. Memento, Strategy, etc.) to provide modularity and abstraction to the code design.",
       languages: "Javascript, HTML, CSS",
       technologies: "MongoDB, ExpressJS, React, NodeJS",
       src: santoriniImage,
@@ -36,7 +36,7 @@ const Portfolio = () => {
     {
       id: 3,
       title: "YUAG SEARCH",
-      description: "An application to search the Yale University Art Gallery archive. Uses AJAX calls for dynamic searching.",
+      description: "An application to search the Yale University Art Gallery archive. Exercises complex SQL queries to search a large database of artwork. Uses asynchronous function calls via AJAX for dynamic searching.",
       src: searchImage,
       url: 'https://github.com/ericwang07/YUAG-Search',
       videoId: "C0q-blGo7eE"
@@ -44,7 +44,7 @@ const Portfolio = () => {
     {
       id: 4,
       title: "YUAG SLIDESHOW",
-      description: "A randomized slideshow featuring the Yale University Art Gallery's collection of artwork. Play/pause and next/previous functionality are supported.",
+      description: "A randomized slideshow featuring the Yale University Art Gallery's collection of artwork. Uses asynchronous function calls via AJAX for constantly-updating time. Play/pause and next/previous functionality are supported.",
       src: slideshowImage,
       url: 'https://github.com/ericwang07/YUAG-Slideshow',
       videoId: "9WuUgT1glmI"
@@ -52,7 +52,7 @@ const Portfolio = () => {
     {
       id: 5,
       title: "BANK SIMULATOR",
-      description: " A basic banking app simulator with GUI. Applying interests/fees must be done manually and is based on the month of most recent transaction. Includes logic that enforces real-world banking constraints.",
+      description: " A basic banking app simulator that demonstrates GUI programming with object-oriented modular design. Includes logic that enforces real-world banking constraints, with the exception that interests/fees must be applied manually and is based on the month of most recent transaction. ",
       src: bankImage,
       url: 'https://github.com/ericwang07/BankSimulator',
       videoId: "ULxMMxSt4nE"
@@ -60,7 +60,7 @@ const Portfolio = () => {
     {
       id: 6,
       title: "GEOGRAPHY GENIUS",
-      description: "A motion-controlled geography quiz game aimed at promoting basic geography knowledge amongst college students as a convenient pastime when walking between classes. Features two different game modes.",
+      description: "A motion-controlled geography quiz game. Teaches basic geography knowledge and promotes movement amongst college students as a convenient pastime when walking between classes. Demonstrates the parsing of input data gathered by a Kinect sensor. Features two different game modes.",
       src: geoImage,
       url: 'https://github.com/coderepic/HCI-Project-Group-31',
       videoId: "X9ToIbh4Czo"
