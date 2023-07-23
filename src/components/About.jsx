01ae860b5103a852594b99532e026e7289230035
ewang001 <eric.y.wang@yale.edu>
2023-07-23 16:47:21 -0700

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
               I cherished the netbook, as it allowed me to indulge in computer-tinkering, a luxury that my parents couldn't afford to provide me.
             </div>
             <div className='font-2xl text-sm pb-4 md:w-1/3 w-2/3 ml-auto mr-0 text-gray-400 flex'>  
-              I quickly to the thrill of maximizing the computational power of limited hardware. By the end of that summer, I had transformed the netbook from a cheap toy to a seriously capable personal device. 
+              I quickly fell in love with the thrill of maximizing the computational power of the limited hardware. By the end of that summer, I had transformed the netbook from a cheap toy to a seriously capable personal device. 
             </div>
             <div className='font-2xl text-sm pb-4 md:w-1/3 w-2/3 ml-auto mr-0 text-gray-400 flex'>  
               For my family business, personal computing unlocked access to an abundance of online wholesale websites. Our profits tripled that year.
